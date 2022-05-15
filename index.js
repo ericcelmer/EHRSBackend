41d9f4258ee46ca807b02a889c8dd5f50b885b46
ericcelmer <eric.celmer@gmail.com>
2022-05-15 14:10:15 -0400

    add APIs

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(
 )
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 app.get('/', (request, response) => {
@@ -20,6 +21,14 @@ app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
 
+app.post('/post-test', (req, res) => {
+  console.log('Got body:', req.body);
+  res.sendStatus(200);
+});
+
 const db = require('./queries')
 app.get('/DiagnosisPrice', db.getDiagnosisPrice)
 app.get('/DiagnosisList', db.getDiagnosisList)
+app.get('/doctorID', db.getDoctorID)
+app.get('/nurseID', db.getNurseID)
+app.post('/order', db.addOrder)
--- queries.js
+++ queries.js
@@ -25,7 +25,39 @@ const getDiagnosisList = (request, response) => {
   })
 }
 
+const getDoctorID = (request, response) => {
+  pool.query("select employeeid from employee where position = 'doctor' limit 1", (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getNurseID = (request, response) => {
+  pool.query("select employeeid from employee where position = 'nurse' limit 1", (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const addOrder = (request, response) => {
+  const {order_id, employeeid, patientid, order_type, order_name} = request.body
+  console.log(request.body)
+  pool.query('INSERT INTO orders (order_id, employeeid, patientid, order_type, order_name) VALUES ($1, $2, $3, $4, $5)', [order_id, employeeid, patientid, order_type, order_name], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send()
+  })
+}
+
   module.exports = {
       getDiagnosisPrice,
-      getDiagnosisList
+      getDiagnosisList,
+      getDoctorID,
+      getNurseID,
+      addOrder
   }
